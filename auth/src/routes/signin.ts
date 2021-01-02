a5f6302a2430ff825fb8bef1704f4f50f711fa46
sumit singh <sumitsingh@globussoft.in>
2021-01-02 11:58:50 +0530

    signin

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,5 +1,10 @@
-import express, { Request, Response } from 'express';
+import jwt from 'jsonwebtoken';
 import { body } from 'express-validator';
+import express, { Request, Response } from 'express';
+
+import { User } from '../models/user';
+import { Password } from '../services/password';
+import { BadRequestError } from '../errors/bad-request-error';
 import { validateRequest } from '../middlewares/validate-request';
 
 const router = express.Router();
@@ -11,7 +16,35 @@ router.post(
         body('password').trim().notEmpty().withMessage('You must enter a password'),
     ],
     validateRequest,
-    (req: Request, res: Response) => {}
+    async (req: Request, res: Response) => {
+        const { email, password } = req.body;
+
+        const existingUser = await User.findOne({ email });
+        if (!existingUser) {
+            throw new BadRequestError('Invalid Credentials');
+        }
+
+        const passwordsMatch = await Password.compare(existingUser.password, password);
+        if (!passwordsMatch) {
+            throw new BadRequestError('Invalid Credentials');
+        }
+
+        // Generate JWT
+        const userJwt = jwt.sign(
+            {
+                id: existingUser.id,
+                email: existingUser.email,
+            },
+            process.env.JWT_KEY!
+        );
+
+        // store it on session object
+        req.session = {
+            jwt: userJwt,
+        };
+
+        res.status(200).send(existingUser);
+    }
 );
 
 export { router as signinRouter };
