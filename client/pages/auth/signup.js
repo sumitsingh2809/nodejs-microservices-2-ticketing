62ee75758e03ff0df7a6dd28577f4383ca03daf8
sumit singh <sumitsingh@globussoft.in>
2021-01-08 11:20:48 +0530

    useRequest Hook

--- client/pages/auth/signup.js
+++ client/pages/auth/signup.js
@@ -1,24 +1,20 @@
 import { useState } from 'react';
 import axios from 'axios';
+import useRequest from '../../hooks/use-request';
 
 export default () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
-    const [errors, setErrors] = useState([]);
+    const { doRequest, errors } = useRequest({
+        url: '/api/users/signup',
+        method: 'POST',
+        body: { email, password },
+    });
 
     const onSubmit = async (event) => {
         event.preventDefault();
 
-        try {
-            const response = await axios.post('/api/users/signup', {
-                email,
-                password,
-            });
-
-            console.log(response.data);
-        } catch (err) {
-            setErrors(err.response.data.errors);
-        }
+        doRequest();
     };
 
     return (
@@ -43,16 +39,8 @@ export default () => {
                 />
             </div>
 
-            {errors.length > 0 && (
-                <div className='alert alert-danger'>
-                    <h4>Ooops....</h4>
-                    <ul className='my-0'>
-                        {errors.map((err) => (
-                            <li key={err.message}>{err.message}</li>
-                        ))}
-                    </ul>
-                </div>
-            )}
+            {errors}
+
             <button type='submit' className='btn btn-primary'>
                 Sign Up
             </button>
