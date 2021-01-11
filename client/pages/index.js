35b128eef70efdf5dd3793d0766fd56d88ff5a84
sumit singh <sumitsingh@globussoft.in>
2021-01-11 13:30:55 +0530

    cross namespace service communication

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,3 +1,34 @@
-export default () => {
+import axios from 'axios';
+
+const LandingPage = ({ currentUser }) => {
+    console.log('i am in the component', currentUser);
     return <h1>Landing Page</h1>;
 };
+
+LandingPage.getInitialProps = async ({ req }) => {
+    if (typeof window === 'undefined') {
+        // We are on the server
+        // run "kubectl get services -n ingress-nginx" to get Services of Ingress-Nginx Namespace
+        // request should be made to http://ingress-nginx-controller.ingress-nginx...
+        // http://SERVICE_NAME.NAMESPACE.svc.cluster.local
+        const { data } = await axios.get(
+            'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser',
+            {
+                headers: req.headers,
+                // headers: {
+                //     Host: 'ticketing.dev',
+                // },
+            }
+        );
+        return data;
+    } else {
+        // We are on the browser
+        // request should be made with a base url of ''
+        const { data } = await axios.get('/api/users/currentuser');
+        return data;
+    }
+
+    return response.data;
+};
+
+export default LandingPage;
