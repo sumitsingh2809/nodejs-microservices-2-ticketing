8bdbcac901309fc4e2578656752504c3ec673e58
sumit singh <sumitsingh@globussoft.in>
2021-02-07 13:01:02 +0530

    create ticket

--- tickets/src/routes/__test__/new.test.ts
+++ tickets/src/routes/__test__/new.test.ts
@@ -1,5 +1,6 @@
 import request from 'supertest';
 import { app } from '../../app';
+import { Ticket } from '../../models/ticket';
 
 it('has a route handler listen to /api/tickets for post requests', async () => {
     const response = await request(app).post('/api/tickets').send({});
@@ -60,13 +61,20 @@ it('returns an error if an invalid price is provided', async () => {
 
 it('creates a ticket with valid inputs', async () => {
     // Add in a check to make sure that the ticket was created
+    let tickets = await Ticket.find({});
+    expect(tickets.length).toEqual(0);
+
+    const title = 'jabdhsk',
+        price = 10;
 
     await request(app)
         .post('/api/tickets')
         .set('Cookie', global.signin())
-        .send({
-            title: 'jabdhsk',
-            price: 10,
-        })
+        .send({ title, price })
         .expect(201);
+
+    tickets = await Ticket.find({});
+    expect(tickets.length).toEqual(1);
+    expect(tickets[0].title).toEqual(title);
+    expect(tickets[0].price).toEqual(price);
 });
